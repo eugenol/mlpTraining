39c7776fa7f959236954442a9bce9ab89ed56b4a
eugenol <eug@live.co.za>
2016-09-12 19:48:33 +0200

    add command line parsing

--- mlpTraining/main.cpp
+++ mlpTraining/main.cpp
@@ -11,16 +11,90 @@ using namespace std;
 using namespace cv;
 using namespace cv::ml;
 
-void get_data(string filename, int num_features, int &num_samples, Mat &_data, Mat &_response);
+bool get_data(string filename, int num_features, int &num_samples, Mat &_data, Mat &_response);
 inline TermCriteria TC(int iters, double eps);
+bool trainMLP();
 
 int main(int argc, char **argv)
+{
+	const String keys =
+		"{help h usage ?	|				| print this message		}"
+		"{train				|				| training mode				}"
+		"{predict			|				| prediction mode			}"
+		"{modelfile			| model.xml     | path to model file        }"
+		"{datafile		    | Results.txt   | path to data file         }"
+		;
+
+	CommandLineParser parser(argc, argv, keys);
+
+	if (parser.has("help"))
+	{
+		cout << "It works" << endl;
+		return 0;
+	}
+
+	return 0;
+}
+
+bool get_data(string filename, int num_features, int &num_samples, Mat &_data, Mat &_response)
+{
+	ifstream file(filename);
+	string str;
+	//int num_samples = 0;
+	//int num_features = 48;
+	int class_count = 3;
+
+	int class_entry;
+	float data_entry;
+
+	Mat response(0, class_count, CV_32F);
+	Mat t_data(0, num_features, CV_32F);
+
+	if (!file)
+	{
+		cout << "Error opening file";
+		return false;
+	}
+
+	while (getline(file, str))
+	{
+		istringstream temp(str);
+		Mat data_row(1, num_features, CV_32F);
+		Mat response_row = Mat::zeros(1, class_count, CV_32F);
+
+		temp >> class_entry;
+		response_row.at<float>(0, class_entry) = 1.f;
+		response.push_back(response_row);
+		//response.push_back((float)class_entry);
+
+		for (int i = 0; i < num_features; ++i)
+		{
+			temp >> data_entry;
+			data_row.at<float>(0, i++) = data_entry;
+		}
+
+		t_data.push_back(data_row);
+	}
+	num_samples = t_data.rows;
+
+	Mat(response).copyTo(_response);
+	Mat(t_data).copyTo(_data);
+
+	return true;
+}
+
+inline TermCriteria TC(int iters, double eps)
+{
+	return TermCriteria(TermCriteria::MAX_ITER + (eps > 0 ? TermCriteria::EPS : 0), iters, eps);
+}
+
+bool trainMLP()
 {
 	int num_samples;
 	int class_count = 3;
 	Mat data;
 	Mat response;
-	get_data("Results.txt",48,num_samples,data, response);
+	get_data("Results.txt", 48, num_samples, data, response);
 
 	Ptr<TrainData> tdata = TrainData::create(data, ROW_SAMPLE, response);
 
@@ -54,51 +128,6 @@ int main(int argc, char **argv)
 	Mat sample = data.row(32);
 	float r = model->predict(sample);
 	cout << r << endl;
-}
 
-void get_data(string filename, int num_features, int &num_samples, Mat &_data, Mat &_response)
-{
-	ifstream file(filename);
-	string str;
-	//int num_samples = 0;
-	//int num_features = 48;
-	int class_count = 3;
-
-	int class_entry;
-	float data_entry;
-
-	Mat response(0, class_count, CV_32F);
-	Mat t_data(0, num_features, CV_32F);
-
-	if (!file)
-		cout << "Error opening file";
-
-	while (getline(file, str))
-	{
-		istringstream temp(str);
-		Mat data_row(1, num_features, CV_32F);
-		Mat response_row = Mat::zeros(1, class_count, CV_32F);
-
-		temp >> class_entry;
-		response_row.at<float>(0, class_entry) = 1.f;
-		response.push_back(response_row);
-		//response.push_back((float)class_entry);
-
-		for (int i = 0; i < num_features; ++i)
-		{
-			temp >> data_entry;
-			data_row.at<float>(0, i++) = data_entry;
-		}
-
-		t_data.push_back(data_row);
-	}
-	num_samples = t_data.rows;
-
-	Mat(response).copyTo(_response);
-	Mat(t_data).copyTo(_data);
-}
-
-inline TermCriteria TC(int iters, double eps)
-{
-	return TermCriteria(TermCriteria::MAX_ITER + (eps > 0 ? TermCriteria::EPS : 0), iters, eps);
+	return 0;
 }
